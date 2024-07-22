b95ba8f46a4f5523bca479e2fdbf3380b614654d
zzzqzhou <ziqi.zhou.cs@gmail.com>
2024-07-22 10:41:47 -0400

    update preprocess

--- preprocess_func.py
+++ preprocess_func.py
@@ -47,6 +47,30 @@ def nonlinear_transformation(slices):
     return slices, nonlinear_slices_1
 
 
+def save_test_npz(data_root, modality, target_root):
+    list_dir = os.listdir(data_root)
+    tbar = tqdm(list_dir, ncols=70)
+    count = 0
+
+    for name in tbar:
+        nib_img = nib.load(os.path.join(data_root, name, name + modality_name_list[modality]))
+        nib_mask = nib.load(os.path.join(data_root, name, name + '_seg.nii'))
+
+        affine = nib_img.affine.copy()
+        
+        slices = nib_img.get_fdata()
+        masks = nib_mask.get_fdata()
+        masks[masks != 0] = 1
+
+        slices = norm(slices)
+
+        if not os.path.exists(os.path.join(target_root, modality)):
+            os.makedirs(os.path.join(target_root, modality))
+        
+        for i in range(slices.shape[2]):
+            save_img(slices[:, :, i], masks[:, :, i], os.path.join(target_root, modality, 'test_sample{}.npz'.format(count)))
+            count += 1
+
 def main(data_root, modality, target_root):
     list_dir = os.listdir(data_root)
     tbar = tqdm(list_dir, ncols=70)
@@ -83,7 +107,13 @@ def main(data_root, modality, target_root):
 
 
 if __name__ == '__main__':
-    data_root = 'Your Nii data Training Folder'
-    target_root = 'Your Npz data Training Folder'
+    data_root = 'Your Nii Training Data Folder'
+    target_root = 'Your Npz Training Data Folder'
     modality = 't2'
     main(data_root, modality, target_root)
+
+    data_root = 'Your Nii Test Data Folder'
+    target_root = 'Your Npz Test Data Folder'
+    modality_list = ['flair', 't1', 't1ce']
+    for modality in modality_list:
+        save_test_npz(data_root, modality, target_root)
